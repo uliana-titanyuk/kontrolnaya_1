ee329d4e25de09118f841824c21f5c3bc7c937f3
uliana-titanyuk <63346238+uliana-titanyuk@users.noreply.github.com>
2020-11-05 20:21:31 +0300

    Update header1.h

--- header1.h
+++ header1.h
@@ -3,20 +3,21 @@
 #include <iostream>
 #include <iostream>
 #include "Header.h"
-#define LENGTH 2
+#define LENGTH 4
 using namespace std;
+
 class Vect {
 private:
-	CRat a[LENGTH];
+	CRat a[LENGTH/2];
 public:
 	Vect() {
-		for (int i = 0; i < LENGTH; i++) {
+		for (int i = 0; i < LENGTH/2; i++) {
 			CRat m;
 			a[i] = m;
 		};
 	}
 	Vect(const CRat *b) {
-		for (int i = 0; i < LENGTH; i++) {
+		for (int i = 0; i < LENGTH/2; i++) {
 			a[i] = b[i];
 		}
 	 }
@@ -25,5 +26,5 @@ public:
   Vect operator-(const Vect& b);
 	
 	void get();
-	void set();
+	Vect& set();
 };
