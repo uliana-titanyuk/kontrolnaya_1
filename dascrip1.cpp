bd5d73cd4c1cfdd436e352288977dd7b5c4b9e8d
uliana-titanyuk <63346238+uliana-titanyuk@users.noreply.github.com>
2020-11-05 20:22:19 +0300

    Update dascrip1.cpp

--- dascrip1.cpp
+++ dascrip1.cpp
@@ -4,21 +4,21 @@
 #include <iostream>
 #include "Header.h"
 Vect& Vect :: operator= (const Vect& b) {
-	for (int i = 0; i < LENGTH; i++) {
+	for (int i = 0; i < LENGTH/2; i++) {
 		this->a[i] = b.a[i];
 	}
 	return *this;                               
 };
 Vect Vect :: operator+(const Vect& b) {
 	Vect m;
-	for (int i = 0; i < LENGTH; i++) {
+	for (int i = 0; i < LENGTH/2; i++) {
 		m.a[i] = a[i] + b.a[i];
 	}
 	return m;
 };
 Vect Vect :: operator-(const Vect& b) {
 	Vect m;
-	for (int i = 0; i < LENGTH; i++) {
+	for (int i = 0; i < LENGTH/2; i++) {
 		m.a[i] = a[i] - b.a[i];
 	}
 	return m;
@@ -26,24 +26,30 @@ Vect Vect :: operator-(const Vect& b) {
 void Vect :: get() {
 	CRat m;
 	cout << "(";
-	for (int i = 0; i < LENGTH - 1; i++) {
+	for (int i = 0; i < LENGTH/2; i++) {
 		m = a[i];
 		m.get();
 		cout << ",";
 	}
 	
 	cout << ")";
+		cout << "\n";
+		cout << "\n";
 };
-void Vect:: set() {
+ Vect& Vect :: set() {
 	CRat a[LENGTH];
-	for (int i = 0; i < 2*LENGTH-1;i+=2) {
-		 CRat m;
-		 int x;
-		 int y;
-		 //this->x ;
-        // this->y ;
-		 m.set(x,y);
-		 a[i]=m;
+	for (int i = 0; i < LENGTH/2; i++) {
+		 int A;
+		 int B;
+		 //inp >> A;
+         //inp >> B;
+        CRat m;
+    
+		 m.set(A,B);
+		  //this->x = x;
+          //this->y = y ;
+		 	this->a[i] = m;
 	}
-	                             
-};
+	return *this; 
+ };
+		 
